e45d68cf5ecd21a2d240142e501fc32c462ea06b
aag <code.aa@gdbl.me>
2016-05-23 17:14:11 +0530

    better version of 2 typed Map

--- slice/slice.go
+++ slice/slice.go
@@ -4,11 +4,9 @@ import "github.com/cheekybits/genny/generic"
 
 type T generic.Type
 type T1 generic.Type
-type T2 generic.Type
 
 type Slice_T []T
 type Slice_T1 []T1
-type Slice_T2 []T2
 
 func (s Slice_T) Map(f func(e T) T) Slice_T {
 	res := []T{}
@@ -18,8 +16,8 @@ func (s Slice_T) Map(f func(e T) T) Slice_T {
 	return res
 }
 
-func (s Slice_T1) Map2(f func(e T1) T2) Slice_T2 {
-	res := []T2{}
+func (s Slice_T) Map_T1(f func(e T) T1) Slice_T1 {
+	res := []T1{}
 	for _, e := range s {
 		res = append(res, f(e))
 	}
